1ef490aa1db587bf0ea361cc0b669940a9426455
Lokesh Majji <lokeshmajji2@gmail.com>
2019-08-07 11:50:39 -0500

    Update SparkJoinFiles.scala

--- SparkJoinFiles.scala
+++ SparkJoinFiles.scala
@@ -1,9 +1,11 @@
 import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.functions._
 
 object SparkJoinFiles extends App {
   System.setProperty("hadoop.home.dir","C:\\Users\\LokeshMajji\\Desktop\\Loki\\Downloads\\Hadoop_WinUtils")
   val spark = new SparkSession.Builder().master("local").appName("Spark Emp").getOrCreate()
   import spark.implicits._
+
   spark.sparkContext.setLogLevel("ERROR")
   val empdf = spark.read.format("csv")
     .option("inferSchema","true")
@@ -25,19 +27,19 @@ object SparkJoinFiles extends App {
   //Using Spark SQL
   empdf.createOrReplaceTempView("employee")
   companydf.createOrReplaceTempView("company")
-  val resultsql = spark.sql("select e.empid,e.empfname,e.emplname,e.salary,e.companyid,c.companyname,e.city,e.state from employee e join company c on e.companyid=c.companyid where e.state='Minnesota' and e.salary > 50000")
+  val resultsql = spark.sql("select c.companyname,count(e.empid) as emp_count from employee e join company c on e.companyid=c.companyid where e.state='Minnesota' and e.salary > 50000 group by c.companyname")
   println("Show Results using Spark SQL")
   resultsql.show()
 
   //using Spark DF
   val joineddf = empdf.join(companydf,empdf.col("companyid") === companydf.col("companyid"), "inner")
-  val resultdf = joineddf.filter("state='Minnesota' and salary > 50000")
+  val resultdf = joineddf.filter("state='Minnesota' and salary > 50000").groupBy("companyname").agg(count("empid"))
   println("Show Results using Spark DF")
   resultdf.show()
 
   //Save as Paruet
-  resultsql.write.format("parquet").mode(SaveMode.Append).save("C:\\Users\\LokeshMajji\\Desktop\\Loki\\Spark2\\out\\parquet")
-  resultsql.write.format("csv").mode(SaveMode.Append).save("C:\\Users\\LokeshMajji\\Desktop\\Loki\\Spark2\\out\\csv")
-  resultsql.write.format("csv").option("sep","\t").mode(SaveMode.Append).save("C:\\Users\\LokeshMajji\\Desktop\\Loki\\Spark2\\out\\tsv")
+  //resultsql.write.format("parquet").mode(SaveMode.Append).save("C:\\Users\\LokeshMajji\\Desktop\\Loki\\Spark2\\out\\parquet")
+  //resultsql.write.format("csv").mode(SaveMode.Append).save("C:\\Users\\LokeshMajji\\Desktop\\Loki\\Spark2\\out\\csv")
+  resultsql.write.format("csv").option("sep","\t").mode(SaveMode.Overwrite).save("C:\\Users\\LokeshMajji\\Desktop\\Loki\\Spark2\\out\\tsv")
 
 }
